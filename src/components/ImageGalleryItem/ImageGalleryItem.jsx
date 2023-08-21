ff8d7690ec1e35d965cbaacd849f694c32b33cc5
ViktoriiaBarma <barma.viktoria@gmail.com>
2023-08-21 09:09:50 +0300

    Update ImageGalleryItem.jsx

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,5 +1,4 @@
 import { ListItem, Picture } from './ImageGalleryItem.styled';
-import PropTypes from 'prop-types';
 
 export const ImageGalleryItem = ({showModal, data}) => {
   const {  tags, webformatURL, largeImageURL } = data;
